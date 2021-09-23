0254cd4028b8d5fcbbd044c0fb34f888d7d52883
Mugnier <a.mugnier@uniandes.edu.co>
2021-09-22 20:46:29 -0500

    Entrega Final RETO-1

--- App/view.py
+++ App/view.py
@@ -39,6 +39,7 @@ def printMenu():
     print()
     print()
     print("Bienvenido")
+    print() 
     print("1- Cargar información en el catálogo")
     print("2- Consultar artistas cronologicamente") # R1
     print("3- Consultar adquisiciones cronologicamente") # R2
@@ -54,7 +55,7 @@ def printEspacio():
     """
 
     print("")
-    print("=" * 50)
+    print("=" * 100)
     print("")
 
 
@@ -96,10 +97,11 @@ def printObrasCronoacq(lista):
     imprime la cantidad de obras adquiridas en un rango de años
     """
     cantidad = lt.size(lista)
-    printEspacio()
+    
     print("Hay " + str(cantidad) + " obras adquiridas en el rago seleccioando")
     print()
     print("Top 3 mas jovenes: ")
+    print()
     for x in range(3):
         elemento = lt.getElement(lista, x)
         print(str(x+1) + ") la obra: " + elemento["name"] + " adquirida en : " + str(elemento["dateacquired"]) + " con medio: " + elemento["medium"] + " y de dimensiones: " +  elemento["dimensions"] +' creada por: ' + str(elemento['artistname']))
@@ -107,6 +109,7 @@ def printObrasCronoacq(lista):
 
     print()
     print("Top 3 mas viejos: ")
+    print()
     for x in range(3):
         elemento = lt.getElement(lista, cantidad-x)
         print(str(x+1) + ") la obra: " + elemento["name"] + " adquirida en : " + str(elemento["dateacquired"]) + " con medio: " + elemento["medium"] + " y de dimensiones: " +  elemento["dimensions"]+ 'creada por: ' + str(elemento['artistname']))
@@ -128,7 +131,7 @@ def print3LastElements(Elements,prop):
 
 
 def printObrasPorTecnica(TotalObras,TotalTecnicas,TecnicaMasUsada,ObrasArtistaTecnica,nombre): 
-    printEspacio()
+    
     if TecnicaMasUsada=='': 
         TecnicaMasUsada='No hay suficiente información '
 
@@ -141,31 +144,32 @@ def printObrasPorTecnica(TotalObras,TotalTecnicas,TecnicaMasUsada,ObrasArtistaTe
     print()
 
     print('Obras con la técnica más utilizada: ')
+    print()
     for i in range(lt.size(ObrasArtistaTecnica)): 
         elemento=lt.getElement(ObrasArtistaTecnica,i)
         print(str(i+1) + ')' + ' La obra: ' + str(elemento['name']) + '  con fecha : '  + str(elemento['date']) + '   dimensiones : ' + str(elemento['dimensions']) + 'y técnica : ' + str(elemento['medium']))
     printEspacio()
 
 def Print_nacionalidad_obras(lista):
-    printEspacio()
+    
 
-    print("Top 10 de nacionalidades con mas artistas")
-
+    print("Top 10 de nacionalidades con mas artistas: ")
+    print()
     for x in range(1, 11):
         elemento = lt.getElement(lista, x)
-        print(elemento["lugar"] + " con " + str(elemento["cantidad"]) + " artistas")
+        print(str(x) + ') ' + elemento["lugar"] + " con " + str(elemento["cantidad"]) + " artistas")
 
     
     print()
 
-    print("De la nacionalidad con mas artistas el top 3 primeros y ultimos")
+    print("De la nacionalidad con mas artistas el top 3 primeros y ultimos: ")
 
     print()
 
     for x in range(5):
         elemento = lt.getElement(lista, 1)
         obra = lt.getElement(elemento["obras"], x)  
-        print("La obra de titulo " + obra["titulo"] + " De artstas " + obra["artistas"] + " Hecha en " + str(obra["fecha"]) + " Con el medio " + obra["medio"] + "Con dimensiones" + obra["dimenciones"])
+        print(str(x+1) + ')' + "La obra de titulo " + obra["titulo"] + " De artstas " + obra["artistas"] + " Hecha en " + str(obra["fecha"]) + " Con el medio " + obra["medio"] + "Con dimensiones" + obra["dimenciones"])
 
     print()
 
@@ -174,12 +178,12 @@ def Print_nacionalidad_obras(lista):
     for x in range(5):
         elemento = lt.getElement(lista, 1)
         obra = lt.getElement(elemento["obras"], cantidad - x)  
-        print("La obra de titulo " + obra["titulo"] + " De artstas " + obra["artistas"] + " Hecha en " + str(obra["fecha"]) + " Con el medio " + obra["medio"] + "Con dimensiones" + obra["dimenciones"])
+        print(str(x+1) + ')' + " La obra de titulo " + obra["titulo"] + " De artstas " + obra["artistas"] + " Hecha en " + str(obra["fecha"]) + " Con el medio " + obra["medio"] + "Con dimensiones" + obra["dimenciones"])
 
     printEspacio()
 
 def printObrasTransporte(TotalObras, TotalPrecio, TotalPeso,TransportePorCosto, TransportePorFecha): 
-    printEspacio()
+    
     print('Hay un total de : ' + str(TotalObras) + ' obras por transportarse, con un precio total de transporte de: ' + str(TotalPrecio) + ' USD, por un peso de: ' + str(TotalPeso) + 'kg')
     print( )
     print('Las 5 obras más costosas por transportar son: ')
@@ -197,30 +201,35 @@ def printObrasTransporte(TotalObras, TotalPrecio, TotalPeso,TransportePorCosto, 
         elemento=lt.getElement(TransportePorFecha,i)
         print(str(i+1) + ')' + ' La obra: ' + str(elemento['name']) + '  con fecha : '  + str(elemento['date']) + '   dimensiones : ' + str(elemento['dimensions']) + ', técnica : ' + str(elemento['medium'])+'y costo de transporte aprox: ' + str(round(elemento['cost']))+' USD')
     print()
-    print()
+    printEspacio()
+    
+
 
 
 def printObrasNuevaEX(lista):
 
-    printEspacio()
+    
 
     print("se llena el espacio con " + str(lt.size(lista) -1) + " obras y " + str(lt.getElement(lista, 0)) + " espcio") 
     
     print()
-    print("primeras 5:")
+    print("Primeras 5:")
+    print()
 
     for x in range(2 ,7):
         obra = lt.getElement(lista, x)
-        print("La obra de titulo " + str(obra["titulo"]) + " hecha por " + obra["nombre"] + " Hecha en " + str(obra["fecha"]) + " Con el medio " + obra["medio"] + " Con dimensiones " + str(obra["dimensiones"]))
+        print(str(x-1) + ') ' + " La obra de titulo " + str(obra["titulo"]) + " hecha por " + obra["nombre"] + " Hecha en " + str(obra["fecha"]) + " Con el medio " + obra["medio"] + " Con dimensiones " + str(obra["dimenciones"]))
 
     print()
-    print("ultimas 5:")
-
+    print("últimas 5:")
+    print()
+    pp=1
     for x in range(lt.size(lista) - 5 ,lt.size(lista)):
         obra = lt.getElement(lista, x)
-        print("La obra de titulo " + str(obra["titulo"]) + " hecha por " + obra["nombre"] + " Hecha en " + str(obra["fecha"]) + " Con el medio " + obra["medio"] + " Con dimensiones " + str(obra["dimensiones"]))
+        print(str(pp) + ') ' +" La obra de titulo " + str(obra["titulo"]) + " hecha por " + obra["nombre"] + " Hecha en " + str(obra["fecha"]) + " Con el medio " + obra["medio"] + " Con dimensiones " + str(obra["dimenciones"]))
+        pp+=1
     
-
+    printEspacio()
     
 
 """
@@ -228,24 +237,35 @@ Menu principal
 """
 while True:
     printMenu()
+    print()
     inputs = input('Seleccione una opción para continuar\n')
     if int(inputs[0]) == 1:
 
-        datatype=input('Seleccione el tipo de tipo de estructura para los datos (ARRAY_LIST, LINKED_LIST)')
-
+        datatype=''
+        
         printEspacio()
         print("Cargando información de los archivos ....")
+
+        
         catalog=initCatalog(datatype)
         loadData(catalog)
 
+        print()
+        
+        print('='*20 + ' RESPUESTA REQ 0 ' + '='*20)
+        print()
+
         print('Obras de arte cargadas ' + str(lt.size(catalog['artworks'])))
         print('Artistas cargados ' + str(lt.size(catalog['artists'])))
-        
+        print()
         print('Úlitmas 3 obras cargadas: ')
         print3LastElements(controller.get3LastElements(catalog['artworks']),'name')
+        print()
         print('Últimos 3 artistas cargados: ')
         print3LastElements(controller.get3LastElements(catalog['artists']),'name')
         printEspacio()
+        
+        
         
         
         
@@ -256,49 +276,94 @@ while True:
         Año_inicial = int(input("Desde que año quieres buscar?:  "))
         Año_fin = int(input("Hasta que año quieres buscar?:  "))
         
-
+        
+        
         cantidadArtistas = controller.artistasCronologico(catalog, Año_inicial, Año_fin)
+        print()
+        
+        print('='*20 + ' RESPUESTA REQ 1 ' + '='*20)
+        print()
+        
         printArtistasCrono(cantidadArtistas)
         printEspacio()
+       
+
+        
         
         
 
     elif int(inputs[0]) == 3:
         FechaInicial = input("desde que fecha quieres buscar?(AAAA-MM-DD):   ")
         FechaFin = input("hasta que fecha quieres buscar?(AAAA-MM-DD):   ")
-        printEspacio()
-
+        print()
+        
+        print('='*20 + ' RESPUESTA REQ 2 ' + '='*20)
+        print()
+       
         CantidadObras=controller.obrasCronologicoacq(catalog,FechaInicial,FechaFin)
+        
         printObrasCronoacq(CantidadObras)
         printEspacio()
+        
 
 
 
 
     elif int(inputs[0]) == 4:
         nombre = input("de que artista deseas buscar?: ")
+        print()
+        
         TotalObras,TotalTecincas,TecnicaMasUsada,ObrasArtistaTecnica=controller.ObrasArtista(catalog,nombre)
+        print()
+        
+        print('='*20 + ' RESPUESTA REQ 3 ' + '='*20)
+        print()
+        
         printObrasPorTecnica(TotalObras,TotalTecincas,TecnicaMasUsada,ObrasArtistaTecnica,nombre)
+       
 
 
     elif int(inputs[0]) == 5:
+        print()
         print("Cargando...")
+        
         Nacionalidad_obras = controller.Nacionalidad_obras(catalog)
+        print()
+        
+        print('='*20 + ' RESPUESTA REQ 4 ' + '='*20)
+        print()
         Print_nacionalidad_obras(Nacionalidad_obras)
+       
 
 
     elif int(inputs[0]) == 6:
         depa = input("Que departamento deseas transportar?: ")
+        
         TotalObras, TotalPrecio,TotalPeso, TransportePorCosto, TransportePorFecha=controller.Transporte(catalog,depa)
-        printObrasTransporte(TotalObras, TotalPrecio,TotalPeso,TransportePorCosto, TransportePorFecha)
+        print()
+        
+        print('='*20 + ' RESPUESTA REQ 5 ' + '='*20)
+        print()
+       
+        printObrasTransporte(TotalObras, TotalPrecio, TotalPeso,TransportePorCosto, TransportePorFecha)
+        
         
 
     elif int(inputs[0]) == 7:
         Año_inicial = input("desde que año inician las obras?: ")
         Año_fin = input("hasta que año van las obras?: ")
         espacio = input("Cual es el area disponible?: ")
+        
         obrasNuevaEx = controller.ObrasNuevaEx(catalog, Año_inicial, Año_fin, espacio)
+
+        print()
+        
+        print('='*20 + ' RESPUESTA REQ 6 ' + '='*20)
+        print()
+        
+       
         printObrasNuevaEX(obrasNuevaEx)
+        
 
 
     else:
--- Docs/readme.txt
+++ Docs/readme.txt
@@ -1,1 +1,7 @@
 Responder preguntas y marcar con el nombre de los integrantes
+
+Est1: David Burgos Mendez - d.burgos - 201818326 – Req 4 
+
+Est2: Andrés Mugnier Zuluaga- a.mugnier- 201729994 - Req 3 
+
+Las preguntas están en el pdf adjunto en la carpeta Docs
